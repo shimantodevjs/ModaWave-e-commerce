551a83d54d745d0317db9c8c4c6d7d7e80edaa29
unknown <shimantodevjs@gmail.com>
2023-07-02 11:26:20 +0300

    pages and components added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,33 @@
+import { useEffect } from "react"
+import{ BrowserRouter,
+        Routes,
+        Route,
+        useLocation} from "react-router-dom"
+import Home from "./pages/home/Home"
+import ItemDetails from "./pages/itemDetails/ItemDetails"
+import Checkout from "./pages/checkout/Checkout"
+import Confirmation from "./pages/checkout/Confirmation"
 
+const ScrollToTop =()=>{
+     const { pathname } = useLocation()
+
+     useEffect(()=>{
+      window.scrollTo(0,0)
+     },[pathname])
+}
+        
 function App() {
   return (
      <div className='app'>
-        
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={ <ItemDetails />} />
+          <Route path="checkout" element={ <Checkout />}/>
+          <Route path="checkout/success" element={<Confirmation />} />
+        </Routes>
+      </BrowserRouter>  
      </div>
   )
 }
